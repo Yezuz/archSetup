1b6f3d3cf3e9675bffeda6ca81c219889d04fc24
Yezuz <jesus.antonio.fuentes.lizama@uabc.edu.mx>
2021-03-11 04:33:01 -0800

    recycler icon added for stagant charge

--- slstatus/config.h
+++ slstatus/config.h
@@ -69,8 +69,8 @@ static const struct arg args[] = {
   { netspeed_rx, " %sB/s] ", "wlan0"},
   { cpu_perc, " [ %s%% | ", NULL},
   { ram_perc, " %s%%] ", NULL},
-  { battery_state, " [  (%s)"  , "BAT0"},
-  { battery_remaining, "%s", "BAT0" },
+  { battery_state, " [%s "  , "BAT0"},
+  { battery_remaining, " %s", "BAT0" },
   { battery_perc, "(%s%%) | ", "BAT0" },
   { datetime, "%s  ]",           "%m.%d %a %H:%M"},
 };
