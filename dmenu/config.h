bc7e84533334519c6761978dff087b30b62c1232
Yezuz <jesus.antonio.fuentes.lizama@uabc.edu.mx>
2021-09-19 00:02:21 -0700

    added screen lock before suspending

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,8 +4,8 @@ static int min_width = 500;  /* minimum width when centered */
 static int fuzzy = 1;        /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"mononoki nerd font:pixelsize=12:antialias=true:autohint=true",
-	"mononoki nerd font:pixelsize=9:antialias=true:autohint=true"
+	"SauceCodePro Nerd Font:pixelsize=12:antialias=true:autohint=true",
+	"Source Code Pro:pixelsize=9:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
